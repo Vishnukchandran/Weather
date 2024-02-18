2f07c5247011384b201251c240b37f5cf9cd031d
Vishnukchandran <vishnuvkc@gmail.com>
2024-02-18 13:31:54 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# Weather App
+
+### 1.Introduction
+
+This README provides documentation for a simple Weather application built with HTML,CSS,JAVASCRIPT and BOOTSTRAP. The application allows to check the weather information by searching the city name.
+
+### 2.Clone the Repository
+
+If you're working with a Git repository, clone it to your local machine using Git commands or download the source code as a ZIP file and extract it to a local directory. Command to clone the repository on the command prompt.
+
+```
+
+git clone 
+
+```
+
+Navigate to the downloaded folder and open the terminal or command prompt on the same folder path, type
+
+```
+cd Weather App
+
+```
+
+Once the folder path is changed to the project folder, use the command below to open VS code editor or you can use other code editor installed on your computer and navigate to the project folder to load the program on the code editor.
+
+```
+code .
+
+```
+### 3.Contributions
+
+Contributions are welcome! Please feel free to submit a pull request or open an issue for any bugs, feature requests, or suggestions.
+
+### 4.Live page
+
+You can access the live page from [here]()
+
+### 5.Credits
+
+Created by VishnuKChandran.
---
+++ js/script.js
@@ -0,0 +1,52 @@
+const apikey = "99e8cd061169796a88f368cfa2e6c297"; //Api key
+const apiUrl = "http://api.openweathermap.org/data/2.5/weather";  //API link
+ 
+//Function to fetch weather data by location
+function getWeather() {
+  const locationInput = document.getElementById("location");
+  const city = locationInput.value;
+
+  fetchWeather(city)
+    .then((data) => {
+      displayWeather(data);
+    })
+    .catch((error) => {
+      console.error("Error fetching weather data:", error.message);
+    });
+}
+
+// Function to fetch weather data from the API
+function fetchWeather(city) {
+  return new Promise((resolve, reject) => {
+    const url = `${apiUrl}?q=${encodeURIComponent(city)}&appid=${apikey}`;
+    fetch(url)
+      .then((response) => {
+        console.log(response);
+        if (!response.ok) {
+          throw new Error("Failed to fetch data");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        resolve(data);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+}
+
+// Function to display weather 
+function displayWeather(data) {
+  const weatherInfo = document.getElementById("weather-info");
+  weatherInfo.innerHTML = `
+    <h2>Weather Information for ${data.name}</h2>
+    <p>Temperature: ${KtoC(data.main.temp)}°C</p>
+    <p>Description: ${data.weather[0].description}</p>
+  `;
+}
+
+//convert temperature from Kelvin to Celsius
+function KtoC(K) {
+  return Math.round(K - 273.15);
+}
